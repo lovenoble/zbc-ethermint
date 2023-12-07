5c6575a6cb317a0eec362a785c5ef839deff630a
Levent Demir <levent.demir@zama.ai>
2023-12-07 15:52:54 +0100

    fix: run full test suite for e2e test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cp .env.example .env
-# TEST_CONTAINER_NAME=ethermintnodelocal0 npm run test:inband
-TEST_CONTAINER_NAME=ethermintnodelocal0 npx hardhat test --grep "should mint the contract" 
+TEST_CONTAINER_NAME=ethermintnodelocal0 npm run test:inband
