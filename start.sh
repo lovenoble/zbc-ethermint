e2938a5d46dd40cb819a3902f9bab15534e12af6
youben11 <ayoub.benaissa@zama.ai>
2023-12-12 15:11:33 +0100

    chore: add a start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TRACE="--trace"
+# TRACE=""
+HOME_ETHERMINTD="$HOME/.ethermintd"
+ETHERMINTD="ethermintd"
+
+LOGLEVEL="info"
+# Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+$ETHERMINTD start --pruning=nothing $TRACE --log_level $LOGLEVEL \
+        --minimum-gas-prices=0.0001aphoton \
+        --json-rpc.gas-cap=50000000 \
+        --json-rpc.api eth,txpool,net,web3
