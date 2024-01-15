c60526debdadc9fee3dbc84eaf4010c9d1a1510e
youben11 <ayoub.benaissa@zama.ai>
2024-01-15 17:13:50 +0100

    fix: forward shanghai and cancun times

--- x/evm/types/chain_config.go
+++ x/evm/types/chain_config.go
@@ -31,27 +31,36 @@ import (
 // EthereumConfig returns an Ethereum ChainConfig for EVM state transitions.
 // All the negative or nil values are converted to nil
 func (cc ChainConfig) EthereumConfig(chainID *big.Int) *params.ChainConfig {
+	var shanghaiTime *uint64 = nil
+	if cc.ShanghaiBlock != nil {
+		shanghaiTimeValue := getBlockValue(cc.ShanghaiBlock).Uint64()
+		shanghaiTime = &shanghaiTimeValue
+	}
+	var cancunTime *uint64 = nil
+	if cc.CancunBlock != nil {
+		cancunTimeValue := getBlockValue(cc.CancunBlock).Uint64()
+		cancunTime = &cancunTimeValue
+	}
 	return &params.ChainConfig{
-		ChainID:        chainID,
-		HomesteadBlock: getBlockValue(cc.HomesteadBlock),
-		DAOForkBlock:   getBlockValue(cc.DAOForkBlock),
-		DAOForkSupport: cc.DAOForkSupport,
-		EIP150Block:    getBlockValue(cc.EIP150Block),
-		// EIP150Hash:              common.HexToHash(cc.EIP150Hash),
-		EIP155Block:         getBlockValue(cc.EIP155Block),
-		EIP158Block:         getBlockValue(cc.EIP158Block),
-		ByzantiumBlock:      getBlockValue(cc.ByzantiumBlock),
-		ConstantinopleBlock: getBlockValue(cc.ConstantinopleBlock),
-		PetersburgBlock:     getBlockValue(cc.PetersburgBlock),
-		IstanbulBlock:       getBlockValue(cc.IstanbulBlock),
-		MuirGlacierBlock:    getBlockValue(cc.MuirGlacierBlock),
-		BerlinBlock:         getBlockValue(cc.BerlinBlock),
-		LondonBlock:         getBlockValue(cc.LondonBlock),
-		ArrowGlacierBlock:   getBlockValue(cc.ArrowGlacierBlock),
-		GrayGlacierBlock:    getBlockValue(cc.GrayGlacierBlock),
-		MergeNetsplitBlock:  getBlockValue(cc.MergeNetsplitBlock),
-		// ShanghaiBlock:           getBlockValue(cc.ShanghaiBlock),
-		// CancunBlock:             getBlockValue(cc.CancunBlock),
+		ChainID:                 chainID,
+		HomesteadBlock:          getBlockValue(cc.HomesteadBlock),
+		DAOForkBlock:            getBlockValue(cc.DAOForkBlock),
+		DAOForkSupport:          cc.DAOForkSupport,
+		EIP150Block:             getBlockValue(cc.EIP150Block),
+		EIP155Block:             getBlockValue(cc.EIP155Block),
+		EIP158Block:             getBlockValue(cc.EIP158Block),
+		ByzantiumBlock:          getBlockValue(cc.ByzantiumBlock),
+		ConstantinopleBlock:     getBlockValue(cc.ConstantinopleBlock),
+		PetersburgBlock:         getBlockValue(cc.PetersburgBlock),
+		IstanbulBlock:           getBlockValue(cc.IstanbulBlock),
+		MuirGlacierBlock:        getBlockValue(cc.MuirGlacierBlock),
+		BerlinBlock:             getBlockValue(cc.BerlinBlock),
+		LondonBlock:             getBlockValue(cc.LondonBlock),
+		ArrowGlacierBlock:       getBlockValue(cc.ArrowGlacierBlock),
+		GrayGlacierBlock:        getBlockValue(cc.GrayGlacierBlock),
+		MergeNetsplitBlock:      getBlockValue(cc.MergeNetsplitBlock),
+		ShanghaiTime:            shanghaiTime,
+		CancunTime:              cancunTime,
 		TerminalTotalDifficulty: nil,
 		Ethash:                  nil,
 		Clique:                  nil,
